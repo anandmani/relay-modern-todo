2ec68d2de7a08e28cf9696f31982fc62d0acf17f
Anand Mani <cma.camelot@gmail.com>
2017-11-15 23:32:12 +0530

    Saving schema to schema.json

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import { schema, getResolver } from './schema'
 import { MongoClient } from 'mongodb'
+import { graphql } from 'graphql'
+import { introspectionQuery } from 'graphql/utilities'
+import fs from 'fs'
 const mongoUrl = 'mongodb://admin:admin@ds151355.mlab.com:51355/rgr'
 
 let app = express();
@@ -9,14 +12,21 @@ app.use(express.static('public'));
 
 (
   async function connect() {
-    let db = await MongoClient.connect(mongoUrl)
+    try {
+      let db = await MongoClient.connect(mongoUrl)
 
-    app.use('/graphql', graphqlHTTP({
-      schema: schema,
-      rootValue: getResolver(db),
-      graphiql: true
-    }))
-    app.listen(3000, () => console.log("Connected to Mongo \nListening on port 3k"))
+      app.use('/graphql', graphqlHTTP({
+        schema: schema,
+        rootValue: getResolver(db),
+        graphiql: true
+      }))
+      app.listen(3000, () => console.log("Connected to Mongo \nListening on port 3k"))
+
+      let json = await graphql(schema, introspectionQuery)
+      fs.writeFile('schema.json', JSON.stringify(json, null, 2))
+    } catch (err) {
+      throw err
+    }
   }
 )()
 
