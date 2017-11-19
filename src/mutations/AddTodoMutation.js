3c516bdafc1d02abdf131d9a4e20bf158bb53886
Anand Mani <cma.camelot@gmail.com>
2017-11-19 15:28:01 +0530

    Add mutation complete

--- src/mutations/AddTodoMutation.js
+++ src/mutations/AddTodoMutation.js
@@ -28,25 +28,10 @@ export function commit(environment, title) {
       onError: err => console.error(err),
       updater: (proxyStore, data) => {
         const rootProxy = proxyStore.getRoot()
-        console.log("root", rootProxy)
         let appProxy = rootProxy.getLinkedRecord('app')
-        console.log("app", appProxy)
         let todosProxy = appProxy.getLinkedRecords('todos')
-
-        let newId = data.addTodo.id
-        // let newProxy  = proxyStore.get(newId)
-        // let newRecord = newProxy.getLinkedRecord(newProxy.getValue())
         let addTodoProxy = proxyStore.getRootField('addTodo')
-        console.log("addTodoProxy", addTodoProxy)
-
-
-        // appProxy.setLinkedRecords(todosProxy, 'todos')
-        // let todosProxy = appProxy.getValue('todos')
-
-        // let node = proxyStore.create(data.addTodo.id, 'Todo')
-
-        // let todosProxy = appProxy.getLinkedRecords('todos')
-        // let last = todosProxy.pop()
+        appProxy.setLinkedRecords([...todosProxy, addTodoProxy], 'todos')
       }
     }
   )
